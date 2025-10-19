a59c7f13287703e354973d3dea8d95fcb2388ad6
Daniel <danielogrady@gmx.de>
2025-10-19 17:42:13 +0200

    Base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/atelier-ogrady/",
+  //base: "/atelier-ogrady/",
   plugins: [react()],
 })
